f39b7d4d6f263b4e7541203b0f866f26ab5c8ae2
Roger <74184276+roger-boop@users.noreply.github.com>
2023-01-16 09:59:12 +0100

    Add files via upload

--- 2023_self_regulatory_motifs/src/pdb2fasta.py
+++ 2023_self_regulatory_motifs/src/pdb2fasta.py
@@ -8,15 +8,17 @@ import os
 
 d = './data/'
 c = 0
-# for each fine in the directory...
+l = len(os.listdir(d))
+# for each file in the directory...
 for pdbfile in os.listdir(d):
     c+=1
-    print('-------\n', c, '\n-------')
-    print(pdbfile)
+    print('-------\n', c, '/', l, '\n-------')
     # open the pdb file and extract the sequence
     with open(d+pdbfile, 'rt') as f:
         seq = next(SeqIO.parse(f, 'pdb-atom'))
+    seq.id = pdbfile[:-4]
+    seq.description = pdbfile[:-4]
     # write the sequence in the directory ./fastas/ in fasta format
     with open('./fastas/'+pdbfile[:-4]+'.fa', 'w') as outf:
         SeqIO.write(seq, outf, 'fasta')
-    print('./fastas/'+pdbfile[:-4]+'.fa')
+
