128440715368c542923d562c52f3ccdbdb796315
Roger <74184276+roger-boop@users.noreply.github.com>
2023-01-18 15:27:31 +0100

    Add files via upload

--- 2023_self_regulatory_motifs/src/pdb2fasta.py
+++ 2023_self_regulatory_motifs/src/pdb2fasta.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-import sys
 from Bio import SeqIO
-import gzip
 import os
 
 
 d = './data/'
 c = 0
 l = len(os.listdir(d))
+outf = open('./multifasta.fa', 'w')
 # for each file in the directory...
 for pdbfile in os.listdir(d):
     c+=1
@@ -16,9 +15,9 @@ for pdbfile in os.listdir(d):
     # open the pdb file and extract the sequence
     with open(d+pdbfile, 'rt') as f:
         seq = next(SeqIO.parse(f, 'pdb-atom'))
+    # Add header to the fastas
     seq.id = pdbfile[:-4]
     seq.description = pdbfile[:-4]
-    # write the sequence in the directory ./fastas/ in fasta format
-    with open('./fastas/'+pdbfile[:-4]+'.fa', 'w') as outf:
-        SeqIO.write(seq, outf, 'fasta')
+    # write the sequence in the file in fasta format
+    SeqIO.write(seq, outf, 'fasta')
 
