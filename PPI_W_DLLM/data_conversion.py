b2e2f0ba2b5b5ae497c91ed19df8558922029127
Bedinka <balazse1998@gmail.com>
2024-03-20 15:45:29 +0000

    Autoencoder input shape problem

--- PPI_W_DLLM/data_conversion.py
+++ PPI_W_DLLM/data_conversion.py
@@ -1,20 +1,21 @@
-import torch
 import numpy as np
 import feature_extraction
-from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import AveragePooling2D, Masking, LSTM, Concatenate, Dense, Reshape
 from tensorflow.keras.layers import Input, Concatenate, Conv2D, Flatten, Dense, Reshape, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 
 interacting_prot, chains_CA = feature_extraction.main()
 
+
 def create_autoencoder_with_pooling(input_shape, num_classes, latent_dim):
-    matrix_input = Input(shape=input_shape)
-
     class_input = Input(shape=(num_classes,))
+    chain1_input = Input(shape=(input_shape[0]))  
+    chain2_input = Input(shape=(input_shape[0])) 
 
-    pooled_input = AveragePooling2D(pool_size=(2, 2))(matrix_input)
+    pooled_input_chain1 = AveragePooling2D(pool_size=(2, 2))(chain1_input)
+    pooled_input_chain2 = AveragePooling2D(pool_size=(2, 2))(chain2_input)
 
-    concatenated_input = Concatenate()([pooled_input, class_input])
+    concatenated_input = Concatenate()([pooled_input_chain1, pooled_input_chain2, class_input])
 
     encoder_output = GlobalAveragePooling2D()(Conv2D(32, (3, 3), activation='relu')(concatenated_input))
 
@@ -23,50 +24,35 @@ def create_autoencoder_with_pooling(input_shape, num_classes, latent_dim):
     decoder_output = Dense(np.prod(input_shape), activation='sigmoid')(decoder_concatenated_input)
     decoded_output = Reshape(input_shape)(decoder_output)
 
-
-    encoder = Model(inputs=[matrix_input, class_input], outputs=encoder_output)
+    encoder = Model(inputs=[chain1_input, chain2_input, class_input], outputs=encoder_output)
     decoder = Model(inputs=[decoder_input, class_input], outputs=decoded_output)
-    autoencoder = Model(inputs=[matrix_input, class_input], outputs=decoder(encoder([matrix_input, class_input])))
+    autoencoder = Model(inputs=[chain1_input, chain2_input, class_input], outputs=decoder(encoder([chain1_input, chain2_input, class_input])))
 
     return autoencoder
 
-def extract_matrices_and_classes(chains_CA):
-    all_matrices = []
+def extract_classes(chains_CA):
     all_classes = []
     for chain_id, chain in chains_CA.items():
-        print("This is extraction, before append:")
-        all_matrices.extend(chain.distance_matrices_CA_AB)
-        print(chain.distance_matrices_CA_AB)
-        all_matrices.extend(chain.distance_matrices_mean_AB)
-        all_matrices.extend(chain.submatrices)
-        all_classes.extend(chain.__dict__.values())
-    return all_matrices, all_classes
- 
-def main ():
-    all_matrices, all_classes = extract_matrices_and_classes(chains_CA)
-    max_height = max_width = max_channels = 0
-    for matrix in all_matrices:
-            print("This is the all_matrixes first element:")
-            print(matrix)
-            height, width = matrix.shape
-            max_height = max(max_height, height)
-            max_width = max(max_width, width)
-            
+        all_classes.append(chain.__dict__.values())
+    return all_classes
 
-    # input shape using maximum shape
-    input_shape = (max_height, max_width, max_channels)
-    print(input_shape)
-    # number of classes and latent dimension for autoencoder
-    num_classes = len(interacting_prot)         
-    latent_dim = num_classes     
-    print(num_classes)                      
-    # autoencoder model with pooling
-    autoencoder = create_autoencoder_with_pooling(input_shape, num_classes, latent_dim)
+def main():
+    all_classes = extract_classes(chains_CA)
+    # Determine input shapes dynamically
 
-    autoencoder.fit([all_matrices, all_classes], all_matrices, epochs=epochs, batch_size=batch_size)
+    input_shape =  100
+    max_channels = 17
+    input_shapes = (input_shape, max_channels)
+    num_classes = 2
+    latent_dim = 2
 
-    # fixed-size vector representation from the encoder for each PDB file
-    encoded_vectors = autoencoder.get_layer("encoder").predict([all_matrices, all_classes])
+    # Create autoencoder model
+    autoencoder = create_autoencoder_with_pooling(input_shapes, num_classes, latent_dim)
+    autoencoder.compile(optimizer='adam', loss='mse')
+    autoencoder.fit(interacting_prot)
+
+    # Extract encoded vectors
+    encoded_vectors = autoencoder.layers[1].predict()
 
 if __name__ == "__main__":
     main()
