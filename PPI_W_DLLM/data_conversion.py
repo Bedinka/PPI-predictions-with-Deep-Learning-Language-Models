d4eff51edb9a4c7f29842571f76a7602921b15c2
Bedinka <balazse1998@gmail.com>
2024-03-24 22:19:29 +0100

    Multiple Vectors from matrixes

--- PPI_W_DLLM/data_conversion.py
+++ PPI_W_DLLM/data_conversion.py
@@ -15,22 +15,42 @@ os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
 interacting_prot = []
 interacting_prot = feature_extraction.main()
-print(interacting_prot)
 
-for protein in interacting_prot:
+
+def mean_matrix_vec(proteins):
+  for protein in proteins:
     if protein.chainID == 'A':
-    #sub_matrix = np.array([protein.mean_submatrices, protein.ca_submatrices])
-      sub_values_a = np.array([protein.mean_submatrices])
+      #sub_matrix = np.array([protein.mean_submatrices, protein.ca_submatrices])
+      sub_values_a = np.array([protein.mean_submatrices],dtype=np.float32)
       print(sub_values_a.shape)
       re_a = sub_values_a.reshape(-1, 7, 7)
     else: 
-      sub_values_b = np.array([protein.mean_submatrices])
+      sub_values_b = np.array([protein.mean_submatrices], dtype=np.float32)
       print(sub_values_b.shape)
       re_b= sub_values_b.reshape(-1, 7, 7)
+  return re_a, re_b 
+
+def ca_matrix_vec(proteins):
+  for protein in proteins:
+    if protein.chainID == 'A':
+      sub_values_a = np.array([protein.ca_submatrices], dtype=np.float32)
+      print(sub_values_a.shape)
+      re_a = sub_values_a.reshape(-1, 7, 7)
+    else: 
+      sub_values_b = np.array([protein.ca_submatrices], dtype=np.float32)
+      print(sub_values_b.shape)
+      re_b= sub_values_b.reshape(-1, 7, 7)
+  return re_a,re_b 
+
+
+re_a, re_b = mean_matrix_vec(interacting_prot)
+ca_a, ca_b = ca_matrix_vec(interacting_prot)
+final_train_vec =  np.concatenate((re_a, ca_a), axis=0)
+final_test_vec = np.concatenate((re_b, ca_b), axis=0) 
     #sub_matrix_2 = np. array ([[protein.mean_submatrices],[protein.ca_submatrices]], dtype = float )
 
-dist_ca_train = re_a
-dist_ca_test = re_b
+dist_ca_train =  final_train_vec
+dist_ca_test = final_test_vec
 dist_ca_train = dist_ca_train.astype('float32') / 255.
 dist_ca_test = dist_ca_test.astype('float32') / 255.
 print (dist_ca_train.shape)
