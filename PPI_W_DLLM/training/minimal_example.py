565ce9e7f9ea1144b7fb1ecd2d6b889f51d65842
Bedinka <balazse1998@gmail.com>
2024-04-09 18:00:29 +0100

    Transformer implementation

--- PPI_W_DLLM/training/minimal_example.py
+++
@@ -1,9 +0,0 @@
-import cramming
-from transformers import AutoModelForMaskedLM, AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained("pbelcak/UltraFastBERT-1x11-long")
-model = AutoModelForMaskedLM.from_pretrained("pbelcak/UltraFastBERT-1x11-long")
-
-text = "Replace me by any text you'd like."
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
